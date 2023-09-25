593267add7aaeb78bf06c7f5d91a9631371b7fe3
Nyanibah Escoffery <naomi@diamondrecovery.net>
2023-09-24 21:37:16 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    let newarray = []
+    for (let i = 0; i < names.length; i++) {
+        newarray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    
+      }
+      return newarray
+
+}
+
+  function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+        console.log(startingNumber --)
+
+    }
+  }
+
+  countDown(4)
